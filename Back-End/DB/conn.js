25817eba0298971faac77f618ec276f4a5466bb7
akash_ghadge <asghadge6@gmail.com>
2021-05-25 14:01:52 +0530

    db connection and SendMail function is promise now

---
+++ Back-End/DB/conn.js
@@ -0,0 +1,16 @@
+// database connections
+const mongoose = require("mongoose");
+const uri = process.env.MONGO_URI;
+mongoose.connect("mongodb://localhost/email-verification", {
+    useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false
+}).then((data) => {
+    console.log("DB is connected..");
+}).catch((err) => {
+    console.log(err);
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("Database connected sucessfully");
+})
+
+
