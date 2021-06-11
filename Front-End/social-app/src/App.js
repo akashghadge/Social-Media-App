6cb059858ff25cae66c487b861bab0296f36be3e
akash_ghadge <asghadge6@gmail.com>
2021-06-11 18:44:07 +0530

    front-end integration with backeup completed

--- Front-End/social-app/src/App.js
+++ Front-End/social-app/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import MatchOTP from './Components/Auth/MatchOTP';
 import Sign from './Components/Auth/Sign';
-
+import SignIn from "./Components/Auth/SignIn"
 function App() {
   return (
     <>
       <Sign></Sign>
+      {/* <SignIn></SignIn> */}
+      <MatchOTP></MatchOTP>
     </>
   );
 }
--- Front-End/social-app/src/Components/Auth/SignUp.jsx
+++ Front-End/social-app/src/Components/Auth/SignUp.jsx
@@ -4,13 +4,16 @@ import { Save } from "@material-ui/icons"
 // 	https://api.cloudinary.com/v1_1/asghadge
 // social-media
 const SignUp = () => {
+    // setting loading true when we request add new  in database
+    let [isLoading, setLoading] = useState(false);
+
     let [allCurrentData, setAllCurrentData] = useState({
         fname: "",
         lname: "",
         email: "",
         username: "",
         password: "",
-        url: ""
+        PicUrl: ""
     });
     function inputChange(event) {
         const { id, value } = event.target
@@ -22,27 +25,80 @@ const SignUp = () => {
             }
         })
     }
-    let [photo, setPhoto] = useState("");
-    function SendUser(event) { }
+    let [photo, setPhoto] = useState(null);
+    function fileInputChange(e) {
+        setPhoto(e.target.files[0]);
+    }
+
+    function SendUser(event) {
+        setLoading(true);
+        const urlUploadCloud = "https://api.cloudinary.com/v1_1/asghadge/image/upload";
+        const urlServerUploadUser = "http://localhost:5000/api/user/add";
+        let formdata = new FormData();
+        formdata.append("file", photo);
+        formdata.append("upload_preset", "social-media");
+        formdata.append("cloud_name", "asghadge");
+        fetch(urlUploadCloud,
+            {
+                method: "post",
+                body: formdata
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                const id = "PicUrl";
+                const value = data.url;
+                setAllCurrentData((prev) => {
+                    return {
+                        ...prev,
+                        [id]: value
+                    }
+                })
+                const requestOptions = {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(allCurrentData)
+                };
+                fetch(urlServerUploadUser, requestOptions)
+                    .then((res) => res.json())
+                    .then((data) => {
+                        console.log(data);
+                        setLoading(false);
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                        setLoading(false);
+                    })
+            })
+            .catch((err) => {
+                console.log(err)
+                setLoading(false);
+            })
+    }
 
 
     return (
         <>
-            <div className="">
-                <h1 className="SignHeading">Sign Up</h1>
-                <label >First name</label>
-                <input type="text" className="form-control inputFieldSignInUp" id="fname" placeholder="" value={allCurrentData.fname} onChange={inputChange} required style={{ boxShadow: "none" }}></input>
-                <label >Last name</label>
-                <input type="text" className="form-control inputFieldSignInUp" id="lname" placeholder="" value={allCurrentData.lname} onChange={inputChange} style={{ boxShadow: "none" }} required></input>
-                <label >Email</label>
-                <input type="email" className="form-control inputFieldSignInUp" id="email" placeholder="you@example.com" style={{ boxShadow: "none" }} onChange={inputChange} value={allCurrentData.email} required></input>
-                <label >UserName</label>
-                <input type="text" className="form-control inputFieldSignInUp" id="username" placeholder="akash@3" style={{ boxShadow: "none" }} onChange={inputChange} value={allCurrentData.username} required></input>
-                <label >Password</label>
-                <input type="password" className="form-control inputFieldSignInUp" id="password" onChange={inputChange} style={{ boxShadow: "none" }} value={allCurrentData.password} required></input>
+            {
+                (isLoading) ? <h1>Loading ....</h1> :
 
-                <button className="submitButton" onClick={SendUser}><Save></Save></button>
-            </div>
+                    <div className="">
+                        <h1 className="SignHeading">Sign Up</h1>
+                        <lable>Profile Photo</lable>
+                        <input type="file" id="profilePic" onChange={fileInputChange} accept="image/*"></input>
+                        <label >First name</label>
+                        <input type="text" className="form-control" id="fname" placeholder="" value={allCurrentData.fname} onChange={inputChange} required style={{ boxShadow: "none" }}></input>
+                        <label >Last name</label>
+                        <input type="text" className="form-control" id="lname" placeholder="" value={allCurrentData.lname} onChange={inputChange} style={{ boxShadow: "none" }} required></input>
+                        <label >Email</label>
+                        <input type="email" className="form-control" id="email" placeholder="you@example.com" style={{ boxShadow: "none" }} onChange={inputChange} value={allCurrentData.email} required></input>
+                        <label >UserName</label>
+                        <input type="text" className="form-control inputFieldSignInUp" id="username" placeholder="akash@3" style={{ boxShadow: "none" }} onChange={inputChange} value={allCurrentData.username} required></input>
+                        <label >Password</label>
+                        <input type="password" className="form-control inputFieldSignInUp" id="password" onChange={inputChange} style={{ boxShadow: "none" }} value={allCurrentData.password} required></input>
+
+                        <button className="submitButton" onClick={SendUser}><Save></Save></button>
+                    </div>
+            }
         </>
     )
 }
