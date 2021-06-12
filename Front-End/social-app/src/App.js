b1d6d43f3cc3bca67df8794343b020c1cc3b0125
akash_ghadge <asghadge6@gmail.com>
2021-06-12 17:40:55 +0530

    navbar material ui component added

--- Front-End/social-app/src/App.js
+++ Front-End/social-app/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import MatchOTP from './Components/Auth/MatchOTP';
 import Sign from './Components/Auth/Sign';
-import SignIn from "./Components/Auth/SignIn"
+import Navbar from "./Components/Navbar/Navbar";
+import NavbarM from "./Components/Navbar/NavbarM";
+import { Switch, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <Sign></Sign>
-      {/* <SignIn></SignIn> */}
-      <MatchOTP></MatchOTP>
+      {/* <Navbar></Navbar> */}
+      <NavbarM></NavbarM>
+      <Switch>
+        <Route exact path="/sign" component={Sign}></Route>
+        <Route exact path="/match-otp" component={MatchOTP}></Route>
+      </Switch>
     </>
   );
 }
