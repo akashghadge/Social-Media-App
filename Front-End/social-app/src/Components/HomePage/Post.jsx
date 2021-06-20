aa6abaa551e27a4f87b84a5bd83cfe674d95c184
akash_ghadge <asghadge6@gmail.com>
2021-06-20 14:16:11 +0530

    succefully decouple component in two components

--- Front-End/social-app/src/Components/HomePage/Post.jsx
+++ Front-End/social-app/src/Components/HomePage/Post.jsx
@@ -1,21 +1,15 @@
 import React, { useState, useEffect } from "react"
 import { NavLink } from "react-router-dom"
 import axios from "axios"
-import { useSelector } from "react-redux"
-import { useHistory } from "react-router"
+import SinglePost from "./SinglePost"
 const Post = () => {
-    let [flagForReq, setFlag] = useState(0);
-    const LoggedUser = useSelector((state) => {
-        return state.User;
-    })
-    let history = useHistory();
-    if (!LoggedUser._id) {
-        history.push("/profile");
-    }
-    // console.log(LoggedUser);
+    //for storing all fetch post data
     let [totalPosts, setTotalPosts] = useState([]);
+    // for storing state if state change we need to fetch info again so we show user latest info only
+    let [flagForReq, setFlag] = useState(0);
+
+    // this will fetch all data when flagForReq is change
     useEffect(() => {
-        // console.log("use effect triggered !!!!!!!!!!!!!!!");
         const urlForAllPost = "http://localhost:5000/api/post/all";
         axios.post(urlForAllPost, {})
             .then((data) => {
@@ -26,159 +20,18 @@ const Post = () => {
                 console.log(err);
             })
     }, [flagForReq]);
-
-    const addLike = (event, idPost) => {
-        const urlAddLike = "http://localhost:5000/api/post/like/add";
-        let token = localStorage.getItem("token");
-
-        const body = {
-            token: token,
-            idOfPost: idPost
-        };
-        axios.post(urlAddLike, body)
-            .then((data) => {
-                // console.log(data);
-                setFlag(++flagForReq);
-            })
-            .catch((err) => {
-                console.log(err);
-                alert(err);
-            })
-    }
-    const removeLike = (event, idPost) => {
-        const urlRemoveLike = "http://localhost:5000/api/post/like/remove";
-        let token = localStorage.getItem("token");
-        const body = {
-            token: token,
-            idOfPost: idPost
-        };
-        axios.post(urlRemoveLike, body)
-            .then((data) => {
-                // console.log(data);
-                setFlag(++flagForReq);
-            })
-            .catch((err) => {
-                console.log(err);
-                alert(err);
-            })
+    // this function will detect the change of flagForReqstate in child class
+    function handleChangeInPost(flagForReqFromState) {
+        setFlag(flagForReqFromState);
     }
 
-    // comment flag
-    let [commentButton, setCommentButton] = useState(false);
-    let [commentInfo, setCommentInfo] = useState("");
-    let [commentLoading, setCommentLoading] = useState(false);
-    function onCommentButton(e, postId) {
-        setCommentLoading(true);
-        setCommentButton(!commentButton);
-        const urlComment = "http://localhost:5000/api/post/comment";
-        const body = {
-            idOfPost: postId
-        };
-        axios.post(urlComment, body).then((data) => {
-            setCommentInfo(data.data.comments);
-            console.log(commentInfo)
-            setCommentLoading(false);
-        })
-            .catch((err) => {
-                console.log(err)
-                setCommentLoading(false);
-            });
-        console.log(commentButton);
-    }
-
-    // create comment
-    let [isCreateCommentOn, setCreateCommentOn] = useState(false);
-    function createCommentButton(e, id) {
-        setCreateCommentOn(!isCreateCommentOn);
-    }
-    let [newComment, setNewComment] = useState("");
-    function newCommentHandle(e, idOfPost) {
-        setNewComment(e.target.value);
-    }
-
-    // send comment
-    function addNewComment(e, idOfPost) {
-        const urlForAddNewComment = "http://localhost:5000/api/post/comment/add";
-        let token = localStorage.getItem("token");
-        const body =
-        {
-            token: token,
-            idOfPost: idOfPost,
-            text: newComment
-        };
-        axios.post(urlForAddNewComment, body)
-            .then((data) => {
-                console.log(data);
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }
     return (
         <>
-
             {
                 (totalPosts === undefined) ? null : totalPosts.map((val, i, arr) => {
                     return (
                         <>
-                            <div>
-                                <img src={val.PicUrl} width="100px" height="100px" alt="profile-pic"></img>
-                                <h3>{val.desc}</h3>
-                                {/* do in production */}
-                                <NavLink exact to={`profile/${val.postedBy._id}`}>
-                                    <h3>{val.postedBy.username}</h3>
-                                </NavLink>
-                                {/* like section */}
-                                {
-                                    (val.likes.includes(LoggedUser._id))
-                                        ?
-                                        <button onClick={(e) => { removeLike(e, val._id) }}>Unlike :{val.likes.length}</button> :
-                                        <button onClick={(e) => { addLike(e, val._id) }}>Like :{val.likes.length}</button>
-                                }
-                                <br></br>
-                                {/* comment section */}
-                                {
-                                    <button onClick={(e) => {
-                                        onCommentButton(e, val._id)
-                                    }}>Comments : {val.comments.length}</button>
-                                }
-                                {
-                                    (commentButton) ?
-                                        commentLoading ? null :
-                                            commentInfo.map((comment, i) => {
-                                                return (
-                                                    <>
-                                                        <h3>{comment.text}</h3>
-                                                        <NavLink exact to={`profile/${comment.postedBy._id}`}>
-                                                            <p>{comment.postedBy.username}</p>
-                                                        </NavLink>
-                                                        <p>{comment.created}</p>
-                                                    </>
-                                                )
-                                            })
-                                        : null
-                                }
-                                <br></br>
-                                {/* create comment */}
-                                {
-                                    <button onClick={(e) => {
-                                        createCommentButton(e, val._id);
-                                    }}>Create Comment</button>
-                                }
-                                {
-                                    (isCreateCommentOn) ?
-                                        <>
-                                            <input type="text" onChange={newCommentHandle} value={newComment}></input>
-                                            <button onClick={(e) => {
-                                                addNewComment(e, val._id);
-                                            }} >Add Comment</button>
-                                        </>
-                                        : null
-                                }
-                            </div>
-                            <br></br>
-                            <br></br>
-                            <br></br>
+                            <SinglePost val={val} index={i} handleChangeInPost={handleChangeInPost} ></SinglePost>
                         </>
                     )
                 })
--- Front-End/social-app/src/Components/HomePage/SinglePost.jsx
+++ Front-End/social-app/src/Components/HomePage/SinglePost.jsx
@@ -1,8 +1,171 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import { NavLink } from "react-router-dom"
+import axios from "axios"
+import { useSelector } from "react-redux"
+import { useHistory } from "react-router"
 const SinglePost = (props) => {
+    let history = useHistory();
+    // getting current user
+    const LoggedUser = useSelector((state) => {
+        return state.User;
+    })
+    // flag for sending signal to relaod post data
+    let [flagForReq, setFlag] = useState(0);
+    useEffect(() => {
+        props.handleChangeInPost(flagForReq);
+    }, [flagForReq]);
+
+    // add like if user is not logged in or after logged in it refreshes page we not add like other wise
+    // we route it to profile
+    const addLike = (event, idPost) => {
+        if (!LoggedUser._id) {
+            history.push("/profile");
+        }
+        const urlAddLike = "http://localhost:5000/api/post/like/add";
+        let token = localStorage.getItem("token");
+
+        const body = {
+            token: token,
+            idOfPost: idPost
+        };
+        axios.post(urlAddLike, body)
+            .then((data) => {
+                setFlag(++flagForReq);
+            })
+            .catch((err) => {
+                console.log(err);
+                alert(err);
+            })
+    }
+    const removeLike = (event, idPost) => {
+        if (!LoggedUser._id) {
+            history.push("/profile");
+        }
+        const urlRemoveLike = "http://localhost:5000/api/post/like/remove";
+        let token = localStorage.getItem("token");
+        const body = {
+            token: token,
+            idOfPost: idPost
+        };
+        axios.post(urlRemoveLike, body)
+            .then((data) => {
+                setFlag(++flagForReq);
+            })
+            .catch((err) => {
+                console.log(err);
+                alert(err);
+            })
+    }
+    let [commentButton, setCommentButton] = useState(false);
+    let [commentInfo, setCommentInfo] = useState("");
+    let [commentLoading, setCommentLoading] = useState(false);
+    function onCommentButton(e, postId) {
+        setCommentLoading(true);
+        setCommentButton(!commentButton);
+        const urlComment = "http://localhost:5000/api/post/comment";
+        const body = {
+            idOfPost: postId
+        };
+        axios.post(urlComment, body).then((data) => {
+            setCommentInfo(data.data.comments);
+            setCommentLoading(false);
+        })
+            .catch((err) => {
+                console.log(err)
+                setCommentLoading(false);
+            });
+    }
+
+    // create comment
+    let [isCreateCommentOn, setCreateCommentOn] = useState(false);
+    function createCommentButton(e, id) {
+        setCreateCommentOn(!isCreateCommentOn);
+    }
+    let [newComment, setNewComment] = useState("");
+    function newCommentHandle(e, idOfPost) {
+        setNewComment(e.target.value);
+    }
+
+    // send comment
+    function addNewComment(e, idOfPost) {
+        const urlForAddNewComment = "http://localhost:5000/api/post/comment/add";
+        let token = localStorage.getItem("token");
+        const body =
+        {
+            token: token,
+            idOfPost: idOfPost,
+            text: newComment
+        };
+        axios.post(urlForAddNewComment, body)
+            .then((data) => {
+                setFlag(++flagForReq);
+            })
+            .catch((err) => {
+                setFlag(++flagForReq);
+                console.log(err);
+            })
+    }
+
     return (
         <>
-
+            <div>
+                <img src={props.val.PicUrl} width="100px" height="100px" alt="profile-pic"></img>
+                <h3>{props.val.desc}</h3>
+                {/* do in production */}
+                <NavLink exact to={`profile/${props.val.postedBy._id}`}>
+                    <h3>{props.val.postedBy.username}</h3>
+                </NavLink>
+                {/* like section */}
+                {
+                    (props.val.likes.includes(LoggedUser._id))
+                        ?
+                        <button onClick={(e) => { removeLike(e, props.val._id) }}>Unlike :{props.val.likes.length}</button> :
+                        <button onClick={(e) => { addLike(e, props.val._id) }}>Like :{props.val.likes.length}</button>
+                }
+                <br></br>
+                {/* comment section */}
+                {
+                    <button onClick={(e) => {
+                        onCommentButton(e, props.val._id)
+                    }}>Comments : {props.val.comments.length}</button>
+                }
+                {
+                    (commentButton) ?
+                        commentLoading ? null :
+                            commentInfo.map((comment, i) => {
+                                return (
+                                    <>
+                                        <h3>{comment.text}</h3>
+                                        <NavLink exact to={`profile/${comment.postedBy._id}`}>
+                                            <p>{comment.postedBy.username}</p>
+                                        </NavLink>
+                                        <p>{comment.created}</p>
+                                    </>
+                                )
+                            })
+                        : null
+                }
+                <br></br>
+                {/* create comment */}
+                {
+                    <button onClick={(e) => {
+                        createCommentButton(e, props.val._id);
+                    }}>Create Comment</button>
+                }
+                {
+                    (isCreateCommentOn) ?
+                        <>
+                            <input type="text" onChange={newCommentHandle} value={newComment}></input>
+                            <button onClick={(e) => {
+                                addNewComment(e, props.val._id);
+                            }} >Add Comment</button>
+                        </>
+                        : null
+                }
+            </div>
+            <br></br>
+            <br></br>
+            <br></br>
         </>
     )
 }
