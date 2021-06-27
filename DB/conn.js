a55d8737a4cb42f41d974a3d800e0b6d086235d7
akash_ghadge <asghadge6@gmail.com>
2021-06-27 13:22:30 +0530

    mongo uri :)

--- DB/conn.js
+++ DB/conn.js
@@ -1,7 +1,7 @@
 // database connections
 const mongoose = require("mongoose");
 const uri = process.env.MONGO_URI;
-mongoose.connect("mongodb://localhost/social-media-app", {
+mongoose.connect(uri || "mongodb://localhost/social-media-app", {
     useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false
 }).then((data) => {
     console.log("DB is connected..");
